9fa0b9e5e20844841781a1dc44f6849dcc7f84df
mike3.14 <michal.kopczynski3.14@gmail.com>
2024-05-25 17:49:56 +0200

    domain name in envi .prod.ts instead of ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   urls: {
-    backendURL: '3.123.146.102:443',
+    //backendURL: '3.123.146.102:443',
+    backendURL: 'tonalityweb.com:443',
   },
 };
